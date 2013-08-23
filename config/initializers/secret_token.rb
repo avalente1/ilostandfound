cc8ce131bb1061237b29a2baa3329d4496f5568b
avalente1 <avalente111@gmail.com>
2013-08-23 18:36:17 -0500

    First commit.  Fork this shit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ilostandfound::Application.config.secret_key_base = '7d93fc9178a0207c2471b0fe0ab2d7d208c72a841ffeb04400b3c8709ba6bd420523ea62ec4b2c2b4b9e5816735e05f1a97aa6bb18ac46bf51e3cce1bbe308a1'
