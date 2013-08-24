b5ab9cb620cf9a6bbd0a2bda24742efb409fffc7
avalente1 <avalente111@gmail.com>
2013-08-24 15:06:31 -0500

    Fixed fuckers

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :email, :name, :cell_number
 
-  validates :name, presence: true
+  validates :first_name, :last_name, presence: true
   validates :email, :cell_number, presence: true, uniqueness: true
 end
