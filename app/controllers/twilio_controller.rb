ad347508e18a970e340d0b4d0258f53db6096d6f
avalente1 <avalente111@gmail.com>
2013-08-24 14:55:10 -0500

    Stupid errors

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessor :email, :name, :phone
+  attr_accessor :email, :name, :cell_phone
 
   validates :name, presence: true
   validates :email, :cell_phone, presence: true, uniqueness: true
