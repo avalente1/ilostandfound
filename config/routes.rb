d38637e7becc60c0459c7bc18e47218d16bd6510
Koos755 <jadriaanse@me.com>
2013-08-23 19:41:58 -0500

    QR code added to Show page
    Plus a littel styling to show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Ilostandfound::Application.routes.draw do
 
   resources :users
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -43,7 +44,7 @@ Ilostandfound::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
