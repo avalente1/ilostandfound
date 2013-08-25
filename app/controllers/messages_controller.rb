57b42baf87753dad5b92348e3e9ea26676877502
James Ruffer <admin@unixbox.ws>
2013-08-25 12:23:57 -0500

    added twilio and email to messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -14,7 +14,6 @@ class MessagesController < ApplicationController
   def create
     @message = Message.new(message_params)
     @message.owner_id = params[:owner_id]
-
     if current_user.present?
       @message.find_id = current_user.id
       if @message.save
@@ -25,12 +24,17 @@ class MessagesController < ApplicationController
       @finder = User.new(finder_params)
       @finder.password = @finder.first_name.downcase
       @finder.password_confirmation = @finder.first_name.downcase
-
       if @finder.save
         create_qrcode(@finder)
         session[:user_id] = @finder.id
         @message.find_id = @finder.id
         if @message.save
+          client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
+          client.account.sms.messages.create(
+          from: TWILIO_CONFIG['from'],
+          to: User.find_by(id: @message.owner_id).cell_number,
+          body: "Someone has found your #{@message.subject}")
+          Wedeliver.wedeliver_email(@user).deliver
           flash[:notice] = "Thank you, the owner have been notified. Now you can also use iLostAndFound"
           redirect_to user_url(current_user)
         else
