a99128ea8a2a207debeade464c66d42683b274f2
Koos755 <jadriaanse@me.com>
2013-08-25 00:28:28 -0500

    updated qrcreate function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,7 @@ class ApplicationController < ActionController::Base
       # user.save
 
       url = Googl.shorten(user_url(user))
+      user.qr_code_uid = url.short_url[-6..-1]
       user.qrcode = "http://chart.googleapis.com/chart?cht=qr&chs=150x150&choe=UTF-8&chld=H&chl=#{url.short_url}"
       user.save
     end
