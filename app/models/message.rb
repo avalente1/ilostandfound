08d9d30a53db0a03238b4e93d60d2d98529bf36e
avalente1 <avalente111@gmail.com>
2013-08-26 21:30:00 -0500

    Fixed messages model

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,7 +4,7 @@ class Message < ActiveRecord::Base
 
   after_create :add_point
   after_create :twilio_lost_item_notification
-  after_create :email_lead_to_wedeliver
+  # after_create :email_lead_to_wedeliver
 
   def add_point
     @point = Point.new
@@ -16,10 +16,10 @@ class Message < ActiveRecord::Base
     client = Twilio::REST::Client.new(TWILIO_CONFIG['sid'], TWILIO_CONFIG['token'])
     client.account.sms.messages.create(
       from: TWILIO_CONFIG['from'],
-      to: User.find_by(id: @message.owner_id).cell_number,
+      to: User.find_by(id: self.owner_id).cell_number,
       body: "Someone has found your #{@message.subject}")
   end
-  def email_lead_to_wedeliver
-    Wedeliver.wedeliver_email(@user).deliver
-  end
+  # def email_lead_to_wedeliver
+  #   Wedeliver.wedeliver_email(@user).deliver
+  # end
 end
