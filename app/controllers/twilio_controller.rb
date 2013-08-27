0df4d03316212649a8bf11f1ce28085dd91c85f3
avalente1 <avalente111@gmail.com>
2013-08-26 21:42:21 -0500

    Still a problem in production for messages

--- app/controllers/twilio_controller.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-  # attr_accessor :email, :first_name, :last_name, :cell_number
-
-  # validates :first_name, :last_name, presence: true
-  # validates :email, :cell_number, presence: true, uniqueness: true
-end
