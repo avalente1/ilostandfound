1883bb3ad0796f9fa40ca0a5db385953ca2d01ca
avalente1 <avalente111@gmail.com>
2013-08-26 19:18:34 -0500

    Continuing to refactor controllers and models

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -44,39 +44,20 @@ class MessagesController < ApplicationController
     end
   end
 
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to @message, notice: 'Message was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
   def destroy
     @message.destroy
-    respond_to do |format|
-      format.html { redirect_to messages_url }
-      format.json { head :no_content }
+      redirect_to messages_url
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_message
       @message = Message.find(params[:id])
     end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def message_params
       params.require(:message).permit(:find_id, :owner_id, :text, :subject)
     end
-
     def finder_params
-      params.require(:user).permit(:first_name, :last_name, :cell_number, :email, :password, :password_confirmation)
-
+      params.require(:user).permit(:first_name, :last_name, :cell_number, :email, :password, :password_confirmation, :ip_address)
     end
 end
